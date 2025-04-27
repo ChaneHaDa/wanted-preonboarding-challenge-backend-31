8fc65ca18850f2696a010888ad14cee6bc60a321
ChaneHaDa <chane.ha.da@gmail.com>
2025-04-27 23:36:17 +0900

    feat: 공용 response dto 추가

---
+++ ecommerce-api/src/main/java/com/chan/ecommerceapi/dto/respose/ResponseDTO.java
@@ -0,0 +1,14 @@
+package com.chan.ecommerceapi.dto.respose;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ResponseDTO<T> {
+    private boolean success;
+    private T data;
+    private String message;
+}

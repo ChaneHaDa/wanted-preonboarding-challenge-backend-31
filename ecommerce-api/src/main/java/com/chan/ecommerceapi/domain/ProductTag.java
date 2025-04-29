6649c6c2a90ef00c55e63de7298ec947c33814ef
ChaneHaDa <chane.ha.da@gmail.com>
2025-04-29 22:58:46 +0900

    feat: tag추가

---
+++ ecommerce-api/src/main/java/com/chan/ecommerceapi/domain/ProductTag.java
@@ -0,0 +1,26 @@
+package com.chan.ecommerceapi.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Entity
+public class ProductTag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+}
---
+++ ecommerce-api/src/main/java/com/chan/ecommerceapi/domain/Tag.java
@@ -0,0 +1,26 @@
+package com.chan.ecommerceapi.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Entity
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String slug;
+
+    @OneToMany(mappedBy = "tag", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductTag> productTags = new ArrayList<>();
+}

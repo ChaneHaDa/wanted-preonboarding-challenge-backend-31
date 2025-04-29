cdf936af54abb63fb06dd620a33a7e0ba92a49e9
ChaneHaDa <chane.ha.da@gmail.com>
2025-04-29 22:05:22 +0900

    feat: category 추가

---
+++ ecommerce-api/src/main/java/com/chan/ecommerceapi/domain/Category.java
@@ -0,0 +1,33 @@
+package com.chan.ecommerceapi.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String slug;
+    private String description;
+    private Long level;
+    private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children = new ArrayList<>();
+}
---
+++ ecommerce-api/src/main/java/com/chan/ecommerceapi/domain/ProductCategory.java
@@ -0,0 +1,28 @@
+package com.chan.ecommerceapi.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Entity
+public class ProductCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private boolean isPrimary;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
